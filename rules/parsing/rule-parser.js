5b524830861d3d90649f675b79047ca6bf757c96
L-48\Vladimir <vladimir.filipovic@tiacgroup.com>
2023-02-05 18:03:42 +0100

    Implemented and tested expression parsing

--- rules/parsing/rule-parser.js
+++ rules/parsing/rule-parser.js
@@ -1,18 +1,16 @@
+import { ComparisonExpression } from '../Comparison-expression.js'
 import { InclusionExpression } from '../inclusion-expression copy.js'
+import { RangeExpression } from '../range-expression.js'
 import { Token, tokenTypes } from './token.js'
 
 /**
- * This regular expression ignores white-space, matches either an IP address, 
- * an expression starting with a dollar sign, an identifier, a number, or other symbol.
+ * This regular expression ignores white-space and single quotes, matches CIDR notations, IPv4 addresses, 
+ * variable names(${name}), comparison operator, numbers or other symbols
  */
-const tokenRegex = /\b(?:\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\b|\$[A-Za-z_]+\b|\b[A-Za-z_]\w*\b|((\d+))|\S/g
-
-const IP_ADDRESS_PLACEHOLDER = '$ip_address'
-const USER_ROLE_PLACEHOLDER = '$user_role'
-
+const tokenRegex = 
+/==|\b(([2]([0-4][0-9]|[5][0-5])|[0-1]?[0-9]?[0-9])[.]){3}(([2]([0-4][0-9]|[5][0-5])|[0-1]?[0-9]?[0-9]))\b\/\b([0-9]|[12][0-9]|3[0-2])\b|\b(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))\b|\$[A-Za-z_]+\b|\b[A-Za-z_][\w']*\b|((\d+))|\S/g
 export class RuleParser {
   
-    //takes req and user and passed accordingly
   constructor(user,request,text) {
     this.nextTokenIndex = 0
     this.tokenList = []
@@ -20,9 +18,12 @@ export class RuleParser {
     this.user = user
     this.request = request
 
+    this.text = text.replace(/,/g, '')
+    this.text = this.text.replace(/["']/g, '')
+
     while (true) {
-      const match = tokenRegex.exec(text)
-      if (match === null) {
+      const match = tokenRegex.exec(this.text)
+      if (!match) {
         break
       }
       this.tokenList.push(new Token(match[0], match.index))
@@ -60,7 +61,7 @@ export class RuleParser {
     const expectedValues = []
     let currentToken = this.#getCurrentToken()
 
-    while(currentToken.getType() !== tokenTypes.PlaceholderValue) {
+    while(currentToken.getType() !== tokenTypes.IpAddressPlaceholderValue && currentToken.getType() !== tokenTypes.UserRolePlaceholderValue) {
         this.nextTokenIndex++
         if(this.nextTokenIndex == this.tokenList.length) {
             throw new Error('Invalid inclusion expression')
@@ -69,7 +70,7 @@ export class RuleParser {
     }
 
     const value =
-      currentToken.getValue() === USER_ROLE_PLACEHOLDER
+      currentToken.getType() === tokenTypes.UserRolePlaceholderValue
         ? this.user.getRoles()
         : this.request.getIpAddress();
 
@@ -91,11 +92,88 @@ export class RuleParser {
     return new InclusionExpression(value, expectedValues)
   }
 
+  #isIpRangeExpression() {
+    const currentToken = this.#getCurrentToken()
+    const currentTokenIsInclusionOperator = currentToken.getType() === tokenTypes.RangeOperator
+    const currentTokenIsFirst =  this.nextTokenIndex === 0
+
+    return currentTokenIsInclusionOperator && currentTokenIsFirst
+  }
+
+  #parseIpRangeExpression() {
+    const value = this.request.getIpAddress();
+
+    let expectedValue
+    let currentToken = this.#getCurrentToken()
+
+    while(!this.#isEndOfExpression()) {
+        if(currentToken.getType() !== tokenTypes.IpAddressWithCIDR) {
+            expectedValue = currentToken.getValue() 
+        }
+        currentToken.getType() !== tokenTypes.IpAddressWithCIDR
+
+        this.nextTokenIndex++
+
+
+        if(this.nextTokenIndex === this.tokenList.length) {
+            throw new Error('Invalid inclusion expression')
+        }
+
+        currentToken = this.#getCurrentToken()
+    }
+
+    return new RangeExpression(value, expectedValue)
+  }
+
+  #isComparisonExpression() {
+      //Example of valid expression: [index=0]$user_role [index=1]== [index=2]'some_value'
+    const secondToken = this.tokenList[1]
+    const secondTokenIsComparisonOperator = secondToken.getType() === tokenTypes.ComparisonEqualsOperator
+    
+    return secondTokenIsComparisonOperator
+  }
+
+  #parseComparisonExpression() {
+    let currentToken = this.#getCurrentToken()
+
+    const value =
+      currentToken.getType() === tokenTypes.UserRolePlaceholderValue
+        ? this.user.getRoles()
+        : this.request.getIpAddress();
+
+    
+
+    while(currentToken.getType() !== tokenTypes.ExpectedValue) {
+        this.nextTokenIndex++
+
+        if(this.nextTokenIndex === this.tokenList.length) {
+            throw new Error('Invalid inclusion expression')
+        }
+
+        currentToken = this.#getCurrentToken()
+    }
+
+    const expectedValue = currentToken.getValue()
+
+    return new ComparisonExpression(value, expectedValue)
+  }
+
    #parseExpression() {
-    //
     if(this.#isInclusionExpression()) {
         return this.#parseInclusionExpression()
     }
+    if(this.#isIpRangeExpression()) {
+        return this.#parseIpRangeExpression()
+    }
+    if(this.#isComparisonExpression()){
+        return this.#parseComparisonExpression()
+    }
+    throw Error('Unknown expression type')
+  }
+
+  #isEndOfExpression() {
+    const currentToken = this.#getCurrentToken()
+    return currentToken.getType() === tokenTypes.ClosedParenthesis
   }
 
   
--- rules/rule-parser-service.spec.js
+++ rules/rule-parser-service.spec.js
@@ -2,6 +2,8 @@ import { RuleParserService } from './rule-parsing-service.js'
 import {Request, User } from '../rubac/index.js'
 import {RuleParser} from './parsing/rule-parser.js'
 import {InclusionExpression} from './inclusion-expression copy.js'
+import { RangeExpression } from './range-expression.js'
+import { ComparisonExpression } from './Comparison-expression.js'
 
 describe('Rule parser test', () => {
   let ruleParser
@@ -40,4 +42,35 @@ describe('Rule parser test', () => {
     // const isInclussion = expression instanceof InclusionExpression
     expect(expression).toBeInstanceOf(InclusionExpression)
   })
+
+  it('should return ip range expression', () => {
+    const request = new Request('100.100.100.100', '/random/')
+    const user = new User('1')
+
+    const ruleParser = new RuleParser(user, request , `ip_range($ip_address, '100.100.100.1/28')`)
+    const expression = ruleParser.parse()
+
+    expect(expression).toBeInstanceOf(RangeExpression)
+  })
+
+  it('should return comparison expression - user role', () => {
+    const request = new Request('100.100.100.100', '/random/')
+    const user = new User('1')
+
+    const ruleParser = new RuleParser(user, request , `$user_role == 'ADMIN'`)
+    const expression = ruleParser.parse()
+
+    expect(expression).toBeInstanceOf(ComparisonExpression)
+  })
+  it('should return comparison expression - ip address', () => {
+    const request = new Request('100.100.100.100', '/random/')
+    const user = new User('1')
+
+    const ruleParser = new RuleParser(user, request , `$ip_address == '100.100.100.100'`)
+    const expression = ruleParser.parse()
+
+    expect(expression).toBeInstanceOf(ComparisonExpression)
+  })
+
+
 })
